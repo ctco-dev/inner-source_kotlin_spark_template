aa5c71ee6903e0551254442cf0ec6f4b58a10254
antonsjevtusenko <38754310+antonsjevtusenko@users.noreply.github.com>
2018-07-19 11:50:28 +0300

    Removing unneeded code from the template (#28)
    * Removing unneeded code from the template: RemoteDataClient, DataService, Retrofit dependencies
    
    * Fixed conflicts

--- src/main/kotlin/com/home/Context.kt
+++ src/main/kotlin/com/home/Context.kt
@@ -3,33 +3,16 @@ package com.home
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import com.home.integration.DataBaseClient
-import com.home.integration.DataService
-import com.home.integration.RemoteDataClient
 import org.apache.commons.dbcp2.BasicDataSource
-import retrofit2.Retrofit
-import retrofit2.converter.jackson.JacksonConverterFactory
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
-fun api(): Context = Context
+fun context(): Context = Context
 
 /**
  * Note that this is an `object`, so we have a guarantee that this is a Singleton
  */
 object Context {
-    val executorService: ExecutorService = Executors.newFixedThreadPool(10)
-
     val objectMapper: ObjectMapper = ObjectMapper()
             .registerModule(ParameterNamesModule())
-
-    val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("http://localhost:4567/")
-            .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-            .build()
-
-    val dataService: DataService = retrofit.create(DataService::class.java)
-
-    val remoteDataClient: RemoteDataClient = RemoteDataClient(dataService)
 
     private val dataSource: BasicDataSource
         get() {
